2c4753a91fe25d13d440aaedb306522d9a177f15
Martin Barros <martinbarros86@gmail.com>
2023-11-28 20:58:24 -0300

    Fix routes and controller

--- src/routes/products.js
+++ src/routes/products.js
@@ -48,7 +48,7 @@ router.get('/detail/:id', productsController.detail)
 
 /*** EDIT ONE PRODUCT ***/
 router.get('/edit/:id', productsController.edit)
-router.put('/:id', productsController.update)
+router.put('/edit/:id', productsController.update)
 
 /*** DELETE ONE PRODUCT***/
 router.delete('/delete/:id', productsController.destroy)
